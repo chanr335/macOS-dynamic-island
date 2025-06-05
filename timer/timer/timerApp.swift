142257ee799b4a0b2d0e2c06886c873d77554f75
chanr335 <76451685+chanr335@users.noreply.github.com>
2025-06-05 03:06:07 -0400

    swift init

---
+++ timer/timer/timerApp.swift
@@ -0,0 +1,17 @@
+//
+//  timerApp.swift
+//  timer
+//
+//  Created by Ryan Chan on 2025-06-05.
+//
+
+import SwiftUI
+
+@main
+struct timerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
