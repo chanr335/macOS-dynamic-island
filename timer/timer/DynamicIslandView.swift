8192f18596f923223474976f37846c9de83632e2
chanr335 <76451685+chanr335@users.noreply.github.com>
2025-06-21 02:26:55 -0400

    stopwatch ui test success

--- timer/timer/DynamicIslandView.swift
+++ timer/timer/DynamicIslandView.swift
@@ -13,17 +13,55 @@ struct DynamicIslandView: View {
     var body: some View {
         HStack {
             Spacer(minLength: 0)
-            ZStack {
+            ZStack (alignment: .leading){
                 RoundedRectangle(cornerRadius: 10)
                     .fill(Color.black)
                     .frame(width: state.isWindowOpen ? 600 : 100, height: 37)
-                    .animation(.easeInOut(duration: 0.5), value: state.isWindowOpen)
+                    .animation(.easeInOut(duration: 0.3), value: state.isWindowOpen)
                     .onTapGesture {
-                        state.isWindowOpen.toggle()
+                        withAnimation(.easeInOut(duration: 0.3)) {
+                            state.isWindowOpen.toggle()
+                        }
                     }
 
+
                 if state.isWindowOpen {
-                    // Your expanded content here
+                    HStack{
+                        Text(String(format:"%02.0f:%02.0f:%02.0f:%02.0f", hour, min, sec, hundredth))
+                            .font(.largeTitle)
+                            .transition(.opacity)
+                            .padding(.leading, 20)
+
+                        Spacer()
+
+                        Button("Start"){
+                            if !start{
+                                start = true
+                                timer = Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true){ _ in
+                                    hundredth += 1
+                                    if hundredth == 100{
+                                        sec += 1
+                                        hundredth = 0
+                                    }
+                                }
+                            }
+                        }
+                        
+                        Button("Stop"){
+                            start = false
+                            timer?.invalidate()
+                        }
+                        
+                        Button("Reset"){
+                            start = false
+                            timer?.invalidate()
+
+                            hour = 0
+                            min = 0
+                            sec = 0
+                            hundredth = 0
+                        }.padding(.trailing, 10)
+                    }
                 }
             }
             Spacer(minLength: 0)
